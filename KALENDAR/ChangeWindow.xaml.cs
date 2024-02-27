b47fde1f1e1e97870c3c83d3f7afa3d184c3be67
snekthesnake <autistgera3@gmail.com>
2024-02-27 19:11:52 +0300

    added page

--- KALENDAR/ChangeWindow.xaml.cs
+++ KALENDAR/ChangeWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace KALENDAR
 {
@@ -22,6 +24,41 @@ namespace KALENDAR
         public ChangeWindow()
         {
             InitializeComponent();
+            DateTime a = DateTime.Now;
+            string b = a.ToString("d");
+            Console.WriteLine(b);
+            int year = DateTime.Now.Year;
+            int month = DateTime.Now.Month;
+            int day = DateTime.Now.Day;
+
+            int nextyear = year + 1;
+
+            DateTime start = new DateTime(year, month, 1, 3, 0, 0); // год, месяц, число, день, час, минута.
+            DateTime end = new DateTime(nextyear, month, day, 3, 0, 0);
+
+            dp.DisplayDateStart = start;
+            dp.DisplayDateEnd = end;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string a = dp.Text;
+            string b = NameEvent_TB.Text;
+            string c = DescEvent_TB.Text;
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=stud-mssql.sttec.yar.ru,38325;user id=user212_db;password=user212;Initial Catalog=user212_db;Trusted_Connection=False;");//Соединение
+                con.Open();
+                SqlCommand com1 = new SqlCommand($"INSERT INTO KALENDAR(Name, Description, Date) VALUES ('{b}','{c}','{a}')", con);
+                com1.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Успешно!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
         }
     }
 }
